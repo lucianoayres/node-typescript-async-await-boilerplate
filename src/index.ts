193030fbdfc82dc10cf8193d86eb102ed15f1563
Luciano Ayres <lucianofarias@gmail.com>
2021-10-29 18:21:25 -0300

    replace dummy API request

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-import { apiGetPosts } from './api.js'
+import axios from 'axios'
+
+interface ITodo {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
+
+async function apiGetPosts(): Promise<ITodo> {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/todos/1'
+  )
+  const data = await response.data
+  return data
+}
 
 const response = await apiGetPosts()
 
